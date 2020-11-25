e77d90480398ddf48ac85ca5cca374999dcbc745
mooredh <mooredh99@gmail.com>
2020-11-25 01:13:23 +0100

    FIxed bandwidth array reference pass

--- controllers/bandwidth.js
+++ controllers/bandwidth.js
@@ -50,7 +50,7 @@ async function addBandwidthByDate(date, bandwidth) {
     let bandwidthDayArr
 
     if (!bandwidthDay) {
-        bandwidthDayArr = new Array(24).fill([0, 0]);
+        bandwidthDayArr = new Array(24).fill(new Array(2).fill(0));
     } else {
         bandwidthDayArr = [...bandwidthDay.bandwidth];
     }
